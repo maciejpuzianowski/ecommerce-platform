ff504392b2448f7695e9272fb7c858ecac601dc5
Maciej Puzianowski <maciekpuzianowski@gmail.com>
2024-09-25 12:55:23 +0200

    inventory service init

---
+++ services/inventory-service/src/main/java/pl/mp/ecommerce_platform/inventory_service/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.mp.ecommerce_platform.inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
