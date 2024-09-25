bb26592e125cfefc92573504428cca66f72a820b
Maciej Puzianowski <maciekpuzianowski@gmail.com>
2024-09-25 11:05:30 +0200

    eureka server

---
+++ discovery/eureka-server/src/test/java/pl/mp/ecommerce_platform/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mp.ecommerce_platform.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
